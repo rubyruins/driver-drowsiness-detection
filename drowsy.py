0df43b4993292076693808bbb560d3e5382df65a
rubyruins <soumya.parekh@somaiya.edu>
2020-03-13 14:28:49 +0530

    initial commit

---
+++ drowsy.py
@@ -0,0 +1,76 @@
+# USAGE
+# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat
+
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5]) #diagonal distance 1
+    B = dist.euclidean(eye[2], eye[4]) #diagonal distance 2
+    C = dist.euclidean(eye[0], eye[3]) #horix=zontal distance
+    ear = (A + B) / (2.0 * C) #computing EAR
+    return ear
+
+EYE_AR_THRESH = 0.31
+EYE_AR_CONSEC_FRAMES = 48
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+
+print("loading facial landmark predictor...")
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# start the video stream thread
+print(" starting video stream thread...")
+vs = VideoStream(0).start()
+time.sleep(1.0)
+i = 0
+min_ear = 100
+max_ear = 0
+ear = 0
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 0)
+    # loop over the face detections
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        # extract the left and right eye coordinates, then use the coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+        # compute the convex hull for the left and right eye, then visualize each of the eyes
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
